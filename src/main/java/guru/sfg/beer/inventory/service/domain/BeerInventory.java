8bf54198633a7a7ef98471dfdf15eca89de68106
Dinesh A <dinesh.adikesavan@gmail.com>
2024-12-22 19:41:02 -0600

    initial commit

--- src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
@@ -21,7 +21,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import jakarta.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
 
@@ -44,6 +44,8 @@ public class BeerInventory extends BaseEntity{
     }
 
     private UUID beerId;
+
     private String upc;
+
     private Integer quantityOnHand = 0;
 }
---
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -0,0 +1,47 @@
+package guru.sfg.beer.inventory.service.web.mappers;
+
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.web.model.BeerInventoryDto;
+import java.sql.Timestamp;
+import java.util.UUID;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-12-22T19:16:03-0600",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
+)
+@Component
+public class BeerInventoryMapperImpl implements BeerInventoryMapper {
+
+    @Override
+    public BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO) {
+        if ( beerInventoryDTO == null ) {
+            return null;
+        }
+
+        UUID id = null;
+        Long version = null;
+        Timestamp createdDate = null;
+        Timestamp lastModifiedDate = null;
+        UUID beerId = null;
+        String upc = null;
+        Integer quantityOnHand = null;
+
+        BeerInventory beerInventory = new BeerInventory( id, version, createdDate, lastModifiedDate, beerId, upc, quantityOnHand );
+
+        return beerInventory;
+    }
+
+    @Override
+    public BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory) {
+        if ( beerInventory == null ) {
+            return null;
+        }
+
+        BeerInventoryDto beerInventoryDto = new BeerInventoryDto();
+
+        return beerInventoryDto;
+    }
+}
